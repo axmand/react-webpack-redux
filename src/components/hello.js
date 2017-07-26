c3f1995808c5b12315d71e0ae73b42e3ab1387c6
axmand <815966508@qq.com>
2017-07-26 10:15:57 +0800

    resolve reject

--- src/components/hello.js
+++ src/components/hello.js
@@ -1,10 +1,7 @@
 console.log("hello!");
-<<<<<<< HEAD
-console.log("where i am?");fdsfdssdfsdfsd
-=======
+console.log("where i am?");
 console.log("where i am?。 shz.fmy");
 console.log("I am back!");
 console.log("We are back!");
 console.log("We are back again!");
 console.log("Lhl is back!");
->>>>>>> d1fbd9ecea2f695665ab242c1197c4b6b8d82140
