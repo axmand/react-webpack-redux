14cb1ba85c20bc5b9c5ce659a3d2a4f10a8f2a8a
PeterFongWhu <fmyofwhu@outlook.com>
2017-09-12 17:55:18 +0800

    complete form1

--- src/components/obligee/ObligeeTable1.js
+++ src/components/obligee/ObligeeTable1.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import  InputCell from './InputCell'
+import SelectCell from './SelectCell'
 class ObligeeTable1 extends Component {  
   render() {
     //const { value, onShowClick,onCompleteInput,dialogShow } = this.props;
@@ -9,65 +10,66 @@ class ObligeeTable1 extends Component {
             <tbody >
 
             <tr>
-   
-            <td width="9%" rowSpan="5"><InputCell command="changeOwner" name="owner"/></td> 
-            <td width="18%"><p >所有权 </p></td>
-            <td width="72%" colSpan="16"><p >/ </p></td>
+        
+          
+            <td width="9%" rowSpan="5">权利人</td> 
+            <td width="18%"><p >所有权</p></td>
+            <td width="72%" colSpan="16"><InputCell  name="OwnPowerSide" title="所有权" tips="填写。。。"/></td>
         </tr>
             <tr>
-            <td width="18%" rowSpan="4"><InputCell command="changeUser" name="user"/></td>
-            <td width="21%" colSpan="5" rowSpan="4"><p >xxxxxxxxxxxxx</p></td>
+            <td width="18%" rowSpan="4">使用权</td>
+            <td width="21%" colSpan="5" rowSpan="4"><InputCell  name="UsePowerSide" title="使用权" tips="此处填写使用权人信息，如企业名称、个人名称等"/></td>
             <td width="23%" colSpan="7"><p >权利人类型 </p></td>
-            <td width="27%" colSpan="4"><p >企业 </p></td>
+            <td width="27%" colSpan="4"><SelectCell  name="PowerSideType" title="权利人类型" tips="此处选择权利人类型，无法归类的填写其他" items="个人 企业 事业单位 国家机关 其他"/></td>
         </tr>
         <tr>
             <td width="23%" colSpan="7"><p >证件种类 </p></td>
-            <td width="27%" colSpan="4"><p >营业执照 </p></td>
+            <td width="27%" colSpan="4"><SelectCell  name="PowerSideCertificateType" title="证件种类" tips="此处选择证件种类" items="身份证 户口本 营业执照 学生证"/></td>
         </tr>
         <tr>
             <td width="23%" colSpan="7"><p >证件号 </p></td>
-            <td width="27%" colSpan="4"><p >xxxxxxx</p></td>
+            <td width="27%" colSpan="4"><InputCell  name="PowerSideCertificateCode" title="证件号码" tips="填写证件号码"/></td>
         </tr>
         <tr>
          <td width="23%" colSpan="7"><p >通讯地址 </p></td>
-            <td width="27%" colSpan="4"><p >&nbsp;</p></td>
+            <td width="27%" colSpan="4"><InputCell  name="PowerSideAddress" title="通信地址" tips="填写权利人通信地址"/></td>
         </tr>
         <tr>
     <td width="27%" colSpan="2"><p >权利类型 </p></td>
-    <td width="14%" colSpan="4"><p >国有建设用地使用权 </p></td>
+    <td width="14%" colSpan="4"><SelectCell  name="PowerType" title="权利类型" tips="此处选择权利类型" items="集体土地所有权 国家土地所有权 国有建设用地使用权 宅基地使用权 集体建设用地使用权 土地承包经营权 林地使用权 草原使用权 水域滩涂养殖权"/></td>
     <td width="15%" colSpan="5"><p >权利性质 </p></td>
-    <td width="14%" colSpan="3"><p >出让 </p></td>
+    <td width="14%" colSpan="3"><InputCell  name="PowerCharacter" title="权利性质" tips="填写权利性质"/></td>
     <td width="12%" colSpan="3"><p >土地权属来源证明材料 </p></td>
-    <td width="14%"><p >&nbsp;</p></td>
+    <td width="14%"><InputCell  name="LandPowerCertificatePaper" title="土地权属来源证明材料" tips="填写土地权属来源证明材料名称及其编号"/></td>
   </tr>
   <tr>
     <td width="27%" colSpan="2"><p >坐落 </p></td>
-    <td width="72%" colSpan="16"><p >南宁市五象新区云村路南侧、英岭路东侧 </p></td>
+    <td width="72%" colSpan="16"><InputCell  name="Location" title="坐落" tips="填写土地坐落"/></td>
   </tr>
  <tr>
     <td width="27%" colSpan="2" rowSpan="2"><p >法定代表人 <br />
       或负责人姓名 </p></td>
-    <td width="11%" rowSpan="2"><p >赵勇 </p></td>
+    <td width="11%" rowSpan="2"><InputCell  name="PrincipalName" title="法定代表人或负责人姓名" tips="填写法定代表人或负责人姓名"/></td>
     <td width="11%" colSpan="5"><p >证件种类 </p></td>
-    <td width="23%" colSpan="7"><p >身份证 </p></td>
+    <td width="23%" colSpan="7"><SelectCell  name="PrincipalCertificateType" title="证件种类" tips="此处选择证件种类" items="身份证 户口本 营业执照 学生证"/></td>
     <td width="7%" rowSpan="2"><p >电话 </p></td>
-    <td width="17%" colSpan="2" rowSpan="2"><p >&nbsp;</p></td>
+    <td width="17%" colSpan="2" rowSpan="2"><InputCell  name="PrincipalCertificateTelephone" title="法定代表人或负责人电话号码" tips="填写法定代表人或负责人电话号码"/></td>
   </tr>
   <tr>
     <td width="11%" colSpan="5"><p >证件号 </p></td>
-    <td width="23%" colSpan="7"><p >xxxxxxxxxxxxxxx</p></td>
+    <td width="23%" colSpan="7"><InputCell  name="PrincipalCertificateCode" title="法定代表人或负责人证件号码" tips="填写法定代表人或负责人证件号码"/></td>
   </tr>
   <tr>
     <td width="27%" colSpan="2" rowSpan="2"><p >代理人姓名 </p></td>
-    <td width="11%" rowSpan="2"><p >&nbsp;</p></td>
+    <td width="11%" rowSpan="2"><InputCell  name="ProcuratorName" title="代理人姓名" tips="填写代理人姓名"/></td>
     <td width="11%" colSpan="5"><p >证件种类 </p></td>
-    <td width="23%" colSpan="7"><p >&nbsp;</p></td>
+    <td width="23%" colSpan="7"><SelectCell  name="ProcuratorCertificateType" title="证件种类" tips="此处选择证件种类" items="身份证 户口本 营业执照 学生证"/></td>
     <td width="7%" rowSpan="2"><p >电话 </p></td>
-    <td width="17%" colSpan="2" rowSpan="2"><p >&nbsp;</p></td>
+    <td width="17%" colSpan="2" rowSpan="2"><InputCell  name="ProcuratorCertificateTelephone" title="代理人电话号码" tips="填写代理人电话号码"/></td>
   </tr>
   <tr>
     <td width="11%" colSpan="5"><p >证件号 </p></td>
-    <td width="23%" colSpan="7"><p >&nbsp;</p></td>
+    <td width="23%" colSpan="7"><InputCell  name="ProcuratorCertificateCode" title="代理人证件号码" tips="填写代理人证件号码"/></td>
   </tr>
   
     </tbody>
---
+++ src/components/obligee/SelectCell.js
@@ -0,0 +1,179 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import ReactDOM from 'react-dom'
+import { Provider, connect } from 'react-redux'
+import Dialog, {
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+} from 'material-ui/Dialog';
+import Button from 'material-ui/Button';
+import Input from 'material-ui/Input';
+import List, { ListItem, ListItemText } from 'material-ui/List'
+import { withStyles } from 'material-ui/styles';
+import Menu, { MenuItem } from 'material-ui/Menu';
+
+// Map Redux state to component props
+function mapStateToProps(state,ownProps) {
+  const key =ownProps.name;
+  let obj = {};
+  obj['key2'] = key;
+  obj[key] = state.value[key];
+  return obj;
+}
+
+// Map Redux actions to component props
+function mapDispatchToProps(dispatch) {
+  return {
+//修改命令 修改的字段名 修改字段的值
+    onCompleteInput: (inputData,name) => {
+
+      dispatch({
+        type: "change", 
+        payload: {
+          inputValue: inputData,
+          inputName: name
+
+        }
+      });
+    }
+  }
+}
+
+var options = [
+    
+  ];
+
+class SelectCellUI extends React.PureComponent {
+
+
+    componentDidMount() {
+        // var items = this.props.items;
+        // options=items.split(" "); 
+        
+    }
+
+
+    state = {
+        anchorEl: null,
+        open: false,
+        selectedIndex: 0,
+      };
+
+    button = undefined;
+    
+      handleClickListItem = event => {
+        this.setState({ open: true, anchorEl: event.currentTarget });
+      };
+    
+      handleMenuItemClick = (event, index) => {
+        this.setState({ selectedIndex: index, open: false });
+
+        var inputData =options[index];
+        this.props.onCompleteInput(inputData, this.props.name);
+      };
+    
+      handleRequestClose = () => {
+        this.setState({ open: false });
+      };
+
+
+
+  showInputCell = () => {
+    this.setState({ cellShow: true });
+  }
+  closeInputCell = () => {
+    this.setState({ cellShow: false });
+  }
+  
+
+  shouldComponentUpdate(nextProps,nextStates){
+    const {name,key2}=this.props;
+    if(!key2)
+      return true;
+    return name === key2;
+  }
+
+  render() {
+    const { onCompleteInput, dialogShow, key2, name,title,tips } = this.props;
+    let value ="未填";
+    if(name ===key2){
+      value =this.props[key2]||"未填";
+    }
+    var items = this.props.items;
+    options=items.split(" "); 
+    
+  
+    return (
+        <div width="100%" height="100%">
+  
+          <ListItem button>
+  
+            <ListItemText primary={value} onClick={this.showInputCell} />
+          </ListItem>
+          <Dialog open={this.state.cellShow}>
+            <DialogTitle>
+            {title}
+            </DialogTitle>
+            <DialogContent>
+            <List>
+          <ListItem
+            button
+            aria-haspopup="true"
+            aria-controls="lock-menu"
+           
+            onClick={this.handleClickListItem}
+          >
+            <ListItemText
+              
+              primary={options[this.state.selectedIndex]}
+            />
+          </ListItem>
+        </List>
+        <Menu
+          id="lock-menu"
+          anchorEl={this.state.anchorEl}
+          open={this.state.open}
+          onRequestClose={this.handleRequestClose}
+        >
+          {options.map((option, index) => (
+            <MenuItem
+              key={option}
+              selected={index === this.state.selectedIndex}
+              onClick={event => this.handleMenuItemClick(event, index)}
+            >
+              {option}
+            </MenuItem>
+          ))}
+        </Menu>
+            
+              <br/>
+              提示
+              <br />
+  {tips}
+            </DialogContent>
+            <DialogActions>
+              <Button color="primary" onClick={this.closeInputCell}>
+                确定
+              </Button>
+  
+            </DialogActions>
+          </Dialog>
+  
+        </div>
+      )
+  }
+}
+
+SelectCellUI.propTypes = {
+  
+  onCompleteInput: PropTypes.func.isRequired,
+}
+
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(SelectCellUI)
+
